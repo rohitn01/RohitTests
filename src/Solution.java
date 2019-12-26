fccabab2f0cbefebe3b67a0ca7ad0f69ec8bdf02
rohitn01 <rohitmen01@gmail.com>
2019-12-25 20:15:58 -0600

    Regex

---
+++ src/Solution.java
@@ -0,0 +1,35 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex{
+    private Pattern IPPattern;
+    private Matcher checkIP;
+    public static final String pattern =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+    public void IPCompiler(){
+        IPPattern = Pattern.compile(pattern);
+    }
+    public boolean isIP(final String addy)
+    {
+        checkIP = IPPattern.matcher(addy);
+        return checkIP.matches();
+    }
+}
+
